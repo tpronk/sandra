e63cfc39bebed2ada500ebf661540ca66d8adbf7
tpronk <pronkthomas@gmail.com>
2015-12-07 16:38:37 +0100

    Added data.frame.new and improved documentation

--- src/R/dataFrames.R
+++ src/R/dataFrames.R
@@ -8,6 +8,10 @@
 #' @param postfix  (character) Postfix to add to column names (name becomes name.postfix)
 #' @param not      (vector) Column names that should NOT be changed (get no prefix or postfix)
 #' @return (data.frame) Data with affixed column names
+#' # Create a new data frame
+#' ds = data.frame.new( vars = c( "id", "x", "y", "z" ) );
+#' # Prefix and postfix all names except "id"
+#' data.frame.affixNames( ds, prefix = "pre", postfix = "post", not = "id" );
 data.frame.affixNames = function( ds, prefix = NULL, postfix = NULL, not = c() ) {
   indexNot = names( ds ) %in% not;
   newNames = names( ds )[ !indexNot ];
@@ -29,7 +33,29 @@ data.frame.affixNames = function( ds, prefix = NULL, postfix = NULL, not = c() )
   return( ds );
 }
 
+#' Drops a variable from a data frame
+#'
+#' @param ds       (data.frame) Data frame
+#' @param dropMe   (character) Variable to drop
+#' @return (data.frame) 
+#' @examples
+#' # Create a new data frame
+#' ds = data.frame.new( vars = c( "id", "x", "y", "z ) );
+#' # Drop "id" 
+#' data.frame.dropVar( ds, "id" );
 data.frame.dropVar = function( ds, dropMe ) {
   matches = names( ds ) == dropMe;
   return( ds[ ,!matches ] );
 }  
+
+#' Creates a data frame with zero rows
+#'
+#' @param vars     (vector) Names of variables (columns) in data.frame
+#' @return (data.frame) 
+#' @examples
+#' # See sandra::data.frame.affixNames
+data.frame.new = function( vars = c() ) {
+  output = data.frame( matrix( nrow = 0, ncol = length( vars ) ) );
+  names( output ) = vars;
+  return( output );
+}
